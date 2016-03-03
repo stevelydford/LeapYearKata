18af47bce2872c511d1afc962bc53c9f0b7080fe
Steve Lydford <stevelydford@gmail.com>
2016-03-03 16:42:24 +0000

    Initial commit

---
+++ LeapYear.Tests/LeapYearValidatorShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeapYear.Tests
+{
+    [TestFixture]
+    public class LeapYearValidatorShould
+    {
+        [TestCase(2001, false)]
+        [TestCase(1996, true)]
+        [TestCase(1900, false)]
+        [TestCase(2000, true)]
+        public void ValidateALeapYear(int year, bool isValid)
+        {
+            var leapYearValidator = new LeapYearValidator();
+
+            var result = leapYearValidator.Validate(year);
+
+            Assert.That(result, Is.EqualTo(isValid));
+        }
+    }
+}
---
+++ LeapYear/LeapYearValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace LeapYear
+{
+    public class LeapYearValidator
+    {
+        public bool Validate(int year)
+        {
+            return IsTypicalLeapYear(year) && !IsAnAtypicalCommonYear(year);
+        }
+
+        private static bool IsTypicalLeapYear(int year)
+        {
+            return year % 4 == 0;
+        }
+
+        private static bool IsAnAtypicalCommonYear(int year)
+        {
+            return year % 100 == 0 && year % 400 != 0;
+        }
+    }
+}
